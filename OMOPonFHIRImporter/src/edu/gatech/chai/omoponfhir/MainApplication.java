0e1004179ca28b6535a89a4a6c883f8fa3752fc3
Walter Conway <wconway8@gatech.edu>
2021-03-14 23:07:13 -0400

    Initial Commit

---
+++ OMOPonFHIRImporter/src/edu/gatech/chai/omoponfhir/MainApplication.java
@@ -0,0 +1,22 @@
+package edu.gatech.chai.omoponfhir;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainApplication {
+	
+	public static Map<String, String> idMapping = new HashMap<String, String>();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//directory to load data in
+		//api key
+		//rest
+		
+		
+		
+		
+	}
+
+}
